1066c9f8af4233f5476ec6c3337882aa7e18db51
Dhyey Patel <dhyeyptl3074@gmail.com>
2024-09-12 17:40:57 +0530

    Update ArduinoScript.cpp

--- ArduinoScript.cpp
+++ ArduinoScript.cpp
@@ -6,13 +6,10 @@
 #define poll_ID_5 4
 #define poll_ID_6 5
 #define env_res 7
-
 const int active_sig = 0;
 const int thresh_hold = 0; 
-
 int poll_ID_reading_[] = {active_sig , active_sig , active_sig , active_sig , active_sig, active_sig};
 int poll_ID_[] = {poll_ID_1, poll_ID_2, poll_ID_3, poll_ID_4, poll_ID_5, poll_ID_6};
-
 void setup() {
   Serial.begin(9600);
   pinMode(env_res, INPUT);
@@ -24,19 +21,14 @@ void setup() {
   pinMode(poll_ID_5, INPUT);
   pinMode(poll_ID_6, INPUT);
 }
-
-void loop() {
-  double env_value = analogRead(env_res); // environmental light sensing
-  // compare to thresh hold 
+void loop() { 
     poll_ID_reading_[0]= analogRead(poll_ID_1);
     poll_ID_reading_[1]= analogRead(poll_ID_2);
     poll_ID_reading_[2]= analogRead(poll_ID_3);
     poll_ID_reading_[3]= analogRead(poll_ID_4);
     poll_ID_reading_[4]= analogRead(poll_ID_5);
     poll_ID_reading_[5]= analogRead(poll_ID_6);
-    for(int i = 0; i < 6; i++)
-    {
-      //detecting faulty poll and returning poll ID
+    for(int i = 0; i < 6; i++){
       if(poll_ID_reading_[i]  > active_sig){
         Serial.println(String(poll_ID_[i]) + ",ACTIVE"); // active
         delay(500);
