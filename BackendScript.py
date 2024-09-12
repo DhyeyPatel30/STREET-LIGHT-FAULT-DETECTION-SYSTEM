dd0eb481edc850f84f3172025f17fe7273d2c43c
Dhyey Patel <dhyeyptl3074@gmail.com>
2024-09-12 17:45:31 +0530

    Update BackendScript.py

--- BackendScript.py
+++ BackendScript.py
@@ -3,8 +3,10 @@ import mysql.connector
 import streamlit as st
 import pandas as pd  
 
+# connection to arduino at port com3 at 9600 baudrate
 arduino = serial.Serial('COM3', 9600)  
 
+# establishing connection with database
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -12,14 +14,17 @@ db = mysql.connector.connect(
     database="hackathon"
 )
 
+# creating a cursor for query execution
 cursor = db.cursor()
 
+# dashboard title and headers
 st.title("Street Light Monitoring Dashboard")
 st.header("Fault Detection System")
 st.write("MH040 - Team Electra")
 
 status_table = st.empty()  
 
+# database updation based on activity status of street light
 def update_status(light_number, status):
     
     cursor.execute(f"UPDATE poll_db SET activity_status = '{status}' WHERE pin_ID = {light_number}")
@@ -32,6 +37,8 @@ def update_status(light_number, status):
 
     status_table.dataframe(df)  
 
+# recieving input from arduino through com3
+# updating dashboard with updated data
 while True:
     if arduino.in_waiting > 0:
         line = arduino.readline().decode('utf-8').strip()
